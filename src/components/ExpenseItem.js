c8d37e00a2100bc582e7f717164b3e828547891e
Mitra Nami <mnami@rossvideo.com>
2021-12-27 13:42:30 -0500

    Create ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,25 @@
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  const { date, title, amount } = props;
+
+  const year = date.getFullYear();
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.getDate();
+
+  return (
+    <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price ">${amount}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
