29be592f3ba3ff3796ef8c4c5dabb374052195ca
Mitra Nami <mnami@rossvideo.com>
2021-12-28 17:01:39 -0500

    Change the title of an expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,31 @@
+import { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const { date, title, amount } = props;
+  const {
+    date: expenseDate,
+    title: expenseTitle,
+    amount: expenseAmount,
+  } = props;
+
+  const [title, setTitle] = useState(expenseTitle);
+
+  const clickHandler = () => {
+    console.log("Clicked");
+    setTitle("Title Updated!");
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={date} />
+      <ExpenseDate date={expenseDate} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <div className="expense-item__price ">${amount}</div>
+        <div className="expense-item__price ">${expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
