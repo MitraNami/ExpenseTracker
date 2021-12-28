66cbc7c408243cc63bb3a0b0791dfa0709e0d0b4
Mitra Nami <mnami@rossvideo.com>
2021-12-28 00:41:26 -0500

    Refactor the common css logic to Card component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,18 +1,19 @@
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
   const { items } = props;
 
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {items.map((item) => {
         const { id, title, amount, date } = item;
         return (
           <ExpenseItem key={id} title={title} amount={amount} date={date} />
         );
       })}
-    </div>
+    </Card>
   );
 };
 
