57b2ba630b007829e0ceecc28b18cca48ecca9ca
Mitra Nami <mnami@rossvideo.com>
2021-12-31 10:08:43 -0500

    Display a message when no expenses exists for a selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,17 +10,22 @@ const Expenses = (props) => {
 
   const [year, setYear] = useState("2021");
 
+  const filteredExpenses = items.filter(
+    (item) => item.date.getFullYear().toString() === year
+  );
+
+  let expensesContent = <p>No expenses found!</p>;
+  if (filteredExpenses.length) {
+    expensesContent = filteredExpenses.map((item) => {
+      const { id, title, amount, date } = item;
+      return <ExpenseItem key={id} title={title} amount={amount} date={date} />;
+    });
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter onSelectYear={setYear} selectedYear={year} />
-      {items
-        .filter((item) => item.date.getFullYear().toString() === year)
-        .map((item) => {
-          const { id, title, amount, date } = item;
-          return (
-            <ExpenseItem key={id} title={title} amount={amount} date={date} />
-          );
-        })}
+      {expensesContent}
     </Card>
   );
 };
